724d498edced10d29fdc49288c0c358840f911c7
Мещеряков Денис Владимирович <Meshcheryakov.DV@ruspace.digital>
2022-01-11 18:20:02 +0300

    Finish project

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let textAreaContent = document.querySelector("textarea");
 let upperCaseButton = document.getElementById("upper-case");
 let lowerCaseButton = document.getElementById("lower-case");
 let properCaseButton = document.getElementById("proper-case");
+let sentenceCaseButton = document.getElementById("sentence-case");
 
 upperCaseButton.addEventListener("click", function() {
     textAreaContent.value = textAreaContent.value.toUpperCase();
@@ -14,8 +15,20 @@ properCaseButton.addEventListener("click", function() {
     if(textAreaContent.value.includes(" ")) {
         let textAreaWords = textAreaContent.value.split(" ");
         for (let i = 0; i < textAreaWords.length; i++) {
-            textAreaWords[i] = textAreaWords[i].charAt(0).toUpperCase() + textAreaWords[i].slice(1);
+            textAreaWords[i] = textAreaWords[i].charAt(0).toUpperCase() + textAreaWords[i].slice(1).toLowerCase();
         }
         textAreaContent.value = textAreaWords.join(" ");
+    } else {
+        textAreaContent.value = textAreaContent.value.charAt(0).toUpperCase() + textAreaContent.value.slice(1).toLowerCase();
+    }
+});
+
+sentenceCaseButton.addEventListener("click", function() {
+    if(textAreaContent.value.includes(". ")) {
+        let textAreaWords = textAreaContent.value.split(". ");
+        for (let i = 0; i < textAreaWords.length; i++) {
+            textAreaWords[i] = textAreaWords[i].charAt(0).toUpperCase() + textAreaWords[i].slice(1).toLowerCase();
+        }
+        textAreaContent.value = textAreaWords.join(". ");
     }
 });
